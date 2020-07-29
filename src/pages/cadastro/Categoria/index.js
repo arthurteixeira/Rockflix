3cc899a59f40db6ac42ea5644395f536c71beb87
arthurteixeira <arthurtjardim@gmail.com>
2020-07-29 15:03:13 -0300

    Adicionando useState

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria() {
+
+    const [categorias, setCategorias] = useState(['Teste']);
+    const [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial');
+
     return (
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+            <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
 
             <form>
                 <label>
                     Nome da categoria:
-                    <input type="text" />
+                    <input
+                        type="text"
+                        value={nomeDaCategoria}
+                        onChange={(e) => {
+                            setNomeDaCategoria(e.target.value);
+                        }}
+                    />
                 </label>
 
                 <button>
                     Cadastrar
                 </button>
             </form>
+
+            <ul>
+                {categorias.map((categoria) => {
+                    return (
+                        <li key={categoria}>
+                            {categoria}
+                        </li>
+                    );
+                })}
+            </ul>
 
             <Link to="/">
                 Ir pra home
